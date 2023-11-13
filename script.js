c2329266ca03e29174e53ce78f5addea785a215c
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-11-13 14:31:46 -0500

    Using bootstrap

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 class ImageViewer {
   constructor(
     inputElementId,
-    galleryElementId,
     selectedImageId,
-    rotationSliderId
+    rotationSliderId,
+    prevButtonId,
+    nextButtonId
   ) {
     this.inputElement = document.getElementById(inputElementId);
-    this.galleryElement = document.getElementById(galleryElementId);
     this.selectedImage = document.getElementById(selectedImageId);
     this.rotationSlider = document.getElementById(rotationSliderId);
+    this.prevButton = document.getElementById(prevButtonId);
+    this.nextButton = document.getElementById(nextButtonId);
+    this.images = [];
+    this.imageRotations = {};
+    this.currentIndex = 0;
     this.initialize();
   }
 
@@ -19,38 +24,58 @@ class ImageViewer {
     this.rotationSlider.addEventListener("input", (event) =>
       this.rotateImage(event)
     );
+    this.prevButton.addEventListener("click", () => this.navigateImage(-1));
+    this.nextButton.addEventListener("click", () => this.navigateImage(1));
   }
 
   handleFileSelection(event) {
     const files = event.target.files;
-    this.galleryElement.innerHTML = "";
-    Array.from(files).forEach((file) => {
-      if (file.type.startsWith("image/")) {
-        const img = document.createElement("img");
-        img.src = URL.createObjectURL(file);
-        img.onclick = () => this.selectImage(file);
-        this.galleryElement.appendChild(img);
-      }
+    this.images = Array.from(files).filter((file) =>
+      file.type.startsWith("image/")
+    );
+    this.images.forEach((file, index) => {
+      this.imageRotations[index] = 0; // Initialize rotation state for each image
     });
+    this.currentIndex = 0;
+    this.displayImage();
   }
 
-  selectImage(file) {
-    this.selectedImage.src = URL.createObjectURL(file);
-    this.selectedImage.onload = function () {
-      URL.revokeObjectURL(this.src);
-    };
-    this.rotationSlider.value = 0; // Reset rotation slider
+  displayImage() {
+    if (this.images.length > 0) {
+      const file = this.images[this.currentIndex];
+      const currentRotation = this.imageRotations[this.currentIndex];
+      this.selectedImage.src = URL.createObjectURL(file);
+      this.selectedImage.onload = () => {
+        URL.revokeObjectURL(this.src);
+        this.selectedImage.style.transform = `rotate(${currentRotation}deg)`;
+      };
+      this.rotationSlider.value = currentRotation;
+    }
   }
 
   rotateImage(event) {
     const angle = event.target.value;
     this.selectedImage.style.transform = `rotate(${angle}deg)`;
+    this.imageRotations[this.currentIndex] = angle;
+  }
+
+  navigateImage(direction) {
+    this.saveCurrentImageRotation();
+    this.currentIndex =
+      (this.currentIndex + direction + this.images.length) % this.images.length;
+    this.displayImage();
+  }
+
+  saveCurrentImageRotation() {
+    const currentRotation = parseInt(this.rotationSlider.value, 10);
+    this.imageRotations[this.currentIndex] = currentRotation;
   }
 }
 
 new ImageViewer(
   "directoryInput",
-  "imageGallery",
   "selectedImage",
-  "rotationSlider"
+  "rotationSlider",
+  "prevButton",
+  "nextButton"
 );
