3c6beb02f6047f8a94cac05f912cf9b08015ea1d
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-11-13 10:31:18 -0500

    getting basic functionality working

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 class ImageViewer {
-  constructor(inputElementId, galleryElementId) {
+  constructor(
+    inputElementId,
+    galleryElementId,
+    selectedImageId,
+    rotationSliderId
+  ) {
     this.inputElement = document.getElementById(inputElementId);
     this.galleryElement = document.getElementById(galleryElementId);
+    this.selectedImage = document.getElementById(selectedImageId);
+    this.rotationSlider = document.getElementById(rotationSliderId);
     this.initialize();
   }
 
   initialize() {
     this.inputElement.addEventListener("change", (event) =>
       this.handleFileSelection(event)
+    );
+    this.rotationSlider.addEventListener("input", (event) =>
+      this.rotateImage(event)
     );
   }
 
@@ -16,19 +26,31 @@ class ImageViewer {
     this.galleryElement.innerHTML = "";
     Array.from(files).forEach((file) => {
       if (file.type.startsWith("image/")) {
-        this.displayImage(file);
+        const img = document.createElement("img");
+        img.src = URL.createObjectURL(file);
+        img.onclick = () => this.selectImage(file);
+        this.galleryElement.appendChild(img);
       }
     });
   }
 
-  displayImage(file) {
-    const img = document.createElement("img");
-    img.src = URL.createObjectURL(file);
-    img.onload = function () {
+  selectImage(file) {
+    this.selectedImage.src = URL.createObjectURL(file);
+    this.selectedImage.onload = function () {
       URL.revokeObjectURL(this.src);
     };
-    this.galleryElement.appendChild(img);
+    this.rotationSlider.value = 0; // Reset rotation slider
+  }
+
+  rotateImage(event) {
+    const angle = event.target.value;
+    this.selectedImage.style.transform = `rotate(${angle}deg)`;
   }
 }
 
-new ImageViewer("directoryInput", "imageGallery");
+new ImageViewer(
+  "directoryInput",
+  "imageGallery",
+  "selectedImage",
+  "rotationSlider"
+);
